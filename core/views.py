d811aeee676c072d13f1a3fa20f814b6804581a2
Mxrqzz <rodrigo.marqsrf@gmail.com>
2025-07-04 08:53:44 -0300

    commit backup

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def index(request):
@@ -10,6 +12,22 @@ def about(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        nome = request.POST.get('name')
+        sobrenome = request.POST.get('surname')
+        telefone = request.POST.get('number')
+        email = request.POST.get('email')
+        senha = request.POST.get('password')
+        senha2 = request.POST.get('passwordConfirm')
+        
+        # Validação simples da senha
+        if senha != senha2:
+            messages.error(request, "As senhas não coincidem.")
+            return redirect('register')
+        
+        
+        
+        pass
     return render(request, "core/register.html")
 
 def login(request):
